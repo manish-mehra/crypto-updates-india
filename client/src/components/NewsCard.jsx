b38532e679189dac3d366bedf17e233bc4178c53
manish-mehra <manishmehra1469@gmail.com>
2022-01-24 15:56:16 +0530

    style: news card + responsive

--- client/src/components/NewsCard.jsx
+++ client/src/components/NewsCard.jsx
@@ -6,12 +6,12 @@ export default function NewsCard({data}) {
   return (
           <div className='news-card'>
             <section className="news-card--content">
-              <a className='news-card--heading' href={data.link}>{data.heading}</a>
+              <a className='news-card--heading' href={data.link} target = '_blank'>{data.heading}</a>
               <span className='news-card--date'>{data.time}</span>
               <p className='news-card--desc'>{data.content}</p>
             </section>
 
-            <section>
+            <section className='news-card--imageWrapper'>
               <img src={data?.image} alt={data.image} />
             </section>
         </div>
