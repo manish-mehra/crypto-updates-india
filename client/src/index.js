9c5784ee32208707810fcab633db36405ee67133
manish-mehra <manishmehra1469@gmail.com>
2022-01-25 14:20:13 +0530

    add favicon icon

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Favicon from 'react-favicon'
+
+import favicon from './favicon.ico'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url={favicon} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
